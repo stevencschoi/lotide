b93a57730485848cc0086c32e8e9c7f40c083b2a
Steven Choi <stevencschoi87@gmail.com>
2020-02-03 20:02:35 +0000

    returning first index of array function

---
+++ head.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const head = (array) => {
+  return array[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
