585ef494a5adf56770807c00f5461ad067e58e7a
Steven Choi <stevencschoi87@gmail.com>
2020-02-03 19:52:10 +0000

    completed console assert replica function

---
+++ assertEqual.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
