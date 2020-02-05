663287c0e5006f457a440cdfadcbf7c4b59a54c3
Steven Choi <stevencschoi87@gmail.com>
2020-02-05 21:17:08 +0000

    final solution to index position

--- letterPositions.js
+++ letterPositions.js
@@ -12,19 +12,26 @@ const eqArrays = function(array1, array2) {
   return true;
 }
 
-// return the indices in the string where the character is found
+// return the indices in the string where the character is found - indexOf?
+// return as an array - create empty array within for loop?
+// loop through each character in the string and return each index
+
 const letterPositions = function(sentence) {
   const results = {};
-  // for loop to identify letter in sentence and return index
-  for (let letter in sentence) {
-    if (sentence[letter]) {
-      results[sentence[letter]] = sentence.indexOf(sentence[letter]);
+  // for loop to identify letter in sentence
+  for (let i = 0; i < sentence.length; i++) {
+    // return the index and store in results object
+    // if character in sentence exists in results, add to the array - else, set it to i
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
     }
   }
   delete results[" "];
   return results
 };
 
-console.log(letterPositions("lighthouse in the house"));
+// console.log(letterPositions("lighthouse in the house"));
 console.log(letterPositions("hello"));
 // console.log(assertArraysEqual(letterPositions("hello")),{ h:0,e:1,l:2,3,o:4 })
