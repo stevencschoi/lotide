00b26836434297cb4b2f8c5a4721a058816203eb
Steven Choi <stevencschoi87@gmail.com>
2020-02-06 22:02:43 +0000

    added assert equal scenarios for testing

--- findKey.js
+++ findKey.js
@@ -2,9 +2,6 @@ const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// takes an object and callback function
-// return the first ***key*** with truthy evaluation
-
 // loop through the object to find the value to evaluate
 const findKey = function(obj, fn) {
   for (let [key, value] of Object.entries(obj)) {
@@ -14,11 +11,17 @@ const findKey = function(obj, fn) {
   }
 };
 
-console.log(findKey({
+console.log(assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)); // => "noma"
+}, x => x.stars === 2),"noma")); // => "noma"
+
+console.log(assertEqual(findKey({
+  "Warriors" : { chamionships: 5},
+  "Raptors": { championships: 1},
+  "Mavericks": { championships: 1},
+}, x => x.championships === 1), "Raptors"));
