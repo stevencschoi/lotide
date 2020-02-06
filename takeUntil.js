963e5e93a7f60734627deb3495f519b705fffb8c
Steven Choi <stevencschoi87@gmail.com>
2020-02-06 20:37:48 +0000

    eslint fixes

--- takeUntil.js
+++ takeUntil.js
@@ -7,10 +7,10 @@ const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
-    } 
+    }
   }
   return true;
-}
+};
 
 // slice array when falsy value is reached
 // check each item in array for a condition
