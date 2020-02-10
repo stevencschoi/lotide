2d16b9aeccb5be9cdd83b6c5da3fc7e2291466b3
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 19:01:04 +0000

    refactored tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,10 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const startingNumbers = [7, 23 , 3, 43, 33];
-assertEqual(startingNumbers[3], 43);
+const tail = require("../tail");
+const assert = require("chai").assert;
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns 3 for [2,3]", () => {
+    assert.deepEqual(tail([2, 3]), [3]);
+  });
+});
