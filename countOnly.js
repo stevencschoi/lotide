23eee07a81e308db2dbb8daa6778ad99f5a3b648
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 20:23:43 +0000

    removed commented out test code

--- countOnly.js
+++ countOnly.js
@@ -19,22 +19,4 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-// const firstNames = [
-//   "Karl",
-//   "Salima",
-//   "Agouhanna",
-//   "Fang",
-//   "Kavith",
-//   "Jason",
-//   "Salima",
-//   "Fang",
-//   "Joe"
-// ];
-
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
-
-// assertEqual(result1["Jason"], 1);
-// assertEqual(result1["Karima"], undefined);
-// assertEqual(result1["Fang"], 2);
-
 module.exports = countOnly;
