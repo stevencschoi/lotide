27a43382f623e7ab4f17bd62597896ba5ecd44e6
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 18:47:25 +0000

    updated mocha in json and refactored headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,12 @@
-const assertEqual = require('../assertEqual');
+const assert = require("chai").assert;
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Mercury", "Venus", "Earth"]), "Mercury");
+describe("#head", () => {
+  it ("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
