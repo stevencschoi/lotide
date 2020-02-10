ef7abeb18f2716cde95d343e8f7c12788b5d0f56
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 02:41:06 +0000

    ran eslint and added psuedocode to test code

--- without.js
+++ without.js
@@ -6,7 +6,7 @@ const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
-    } 
+    }
   }
   return true;
 };
@@ -19,13 +19,13 @@ const eqArrays = function(array1, array2) {
 
 const without = function(source, remove) {
 // if element from source array exists (INCLUDED) in remove array, don't (!) return it in filter
-return source.filter(element => !remove.includes(element))
+  return source.filter(element => !remove.includes(element));
 };
 
 console.log(without([1, 2, 3], [1])); // => [2, 3]
 console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-console.log(without([true, false, "false"],[false]));
-console.log(without(["apple", "banana", 3], ["banana"]));
+console.log(without([true, false, "false"],[false])); // => [true, "false"]
+console.log(without(["apple", "banana", 3], ["banana"])); // => ["apple", 3]
 
 const words = ["hello", "world", "lighthouse"];
 without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
