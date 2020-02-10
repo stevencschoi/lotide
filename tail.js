66c35d7511e3c8655eebb2c92f8f4f4be2755845
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 02:15:24 +0000

    added test case for assertEqual

--- tail.js
+++ tail.js
@@ -14,3 +14,6 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const startingNumbers = [7, 23 , 3, 43, 33];
+assertEqual(startingNumbers[3], 43);
