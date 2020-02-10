daa0d8d81ee6343f43e252071dbac1bdb2c6a82c
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 03:28:59 +0000

    ran eslint and updated formatting

--- map.js
+++ map.js
@@ -7,10 +7,10 @@ const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
-    } 
+    }
   }
   return true;
-}
+};
 
 const words = ["ground", "control", "to", "major", "tom"];
 
@@ -20,7 +20,7 @@ const map = function(array, callback) {
     results.push(callback(item));
   }
   return results;
-}
+};
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
