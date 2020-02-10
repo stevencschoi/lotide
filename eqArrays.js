10dd1be990cca8eef19af0be4ae285b67839f5d1
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 02:31:19 +0000

    ran eslint and updated file

--- eqArrays.js
+++ eqArrays.js
@@ -12,7 +12,7 @@ const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
-    } 
+    }
   }
   return true;
 };
@@ -27,4 +27,4 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 // eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
 // eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
