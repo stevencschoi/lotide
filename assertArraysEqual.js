19f82c0df8f082acbf69d3a2df4c9bf451d9321b
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 02:33:55 +0000

    ran eslint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,10 +7,10 @@ const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
-    } 
+    }
   }
   return true;
-}
+};
 
 console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
 console.log(assertArraysEqual([1,2,3],[1,2,3]));
