5aa76493df4c73ceb0ea95bab5680a1917b94d9b
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 02:32:45 +0000

    added pseudocode to eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -4,11 +4,12 @@ const assertEqual = function(actual, expected) {
 
 // receive two arrays as arguments
 const eqArrays = function(array1, array2) {
-  // verify each item at each index of array matches
+  // first verify if array lengths match; if they do not, we can exit the program
   if (array1.length !== array2.length) {
     return false;
   }
   
+  // verify each item at each index of array matches
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
