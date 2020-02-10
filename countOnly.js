33e1ead3a48ee98b550d37f05e6d8c5f0e34f882
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 02:49:29 +0000

    ran eslint to update file

--- countOnly.js
+++ countOnly.js
@@ -1,23 +1,27 @@
 const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-}
+};
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
+  // create empty object to store items and count of each item
   const results = {};
+  // loop through array;
   for (const item of allItems) {
+    //if item exists in array, count++
     if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
+        // if item does not exist in array, add it to the array and set count to 1
         results[item] = 1;
       }
     }
   }
   
   return results;
-}
+};
 
 const firstNames = [
   "Karl",
