18e6887627e85f8d07adf5a3604fecdb55254af4
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 03:19:20 +0000

    ran eslint and updated formatting

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -11,14 +11,14 @@ const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
-    } 
+    }
   }
   return true;
 };
 
 const eqObjects = function(obj1, obj2) {
-  objKeys1 = Object.keys(obj1);
-  objKeys2 = Object.keys(obj2);
+  const objKeys1 = Object.keys(obj1);
+  const objKeys2 = Object.keys(obj2);
   if (objKeys1.length !== objKeys2.length) {
     return false;
   }
@@ -34,11 +34,11 @@ const eqObjects = function(obj1, obj2) {
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual,expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-  console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`)
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
-// assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' });
+assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' });
 // console.log(`Example label: ${inspect(actual)}`);
