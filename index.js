ebda0dc0aceb8f2aed36b56d3af72e99dca9950e
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 21:10:13 +0000

    update index.js file to include refactored function files

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
+const countOnly = require('./countOnly');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
 
 module.exports = {
   head,
   tail,
-  middle
+  middle,
+  countOnly,
+  findKeyByValue,
+  flatten,
+  letterPositions
 };
