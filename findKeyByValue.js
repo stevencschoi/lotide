3a10d104c6cdac566a12f13b203e87aef8d66b61
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 03:07:50 +0000

    ran eslint and rewrote fn to solidify understanding

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,20 +6,20 @@ const assertEqual = function(actual, expected) {
 // loop through the keys using Object.keys(obj)
 
 // check if value exists in object
-// if 
 const findKeyByValue = function(obj, value) {
   // the index represented by keys in object
   const objKeys = Object.keys(obj);
   // loop through obj keys
-  for (const val of objKeys) {
+  for (const key of objKeys) {
+    console.log("the value of objKeys is ",key);
     // if value exists in the given object at "index" of objKeys return
-    if (value === obj[val]) {
-      return val;
+    if (value === obj[key]) {
+      return key;
     }
   }
 };
 
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
