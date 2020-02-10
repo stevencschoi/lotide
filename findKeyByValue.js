001ee6fd56e3d54534a23698c9b75ca72a92c2c4
Steven Choi <stevencschoi87@gmail.com>
2020-02-10 20:42:11 +0000

    refactored findKeyByValue with chai and mocha

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,7 +1,3 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 // if the value exists in the object, return the key of the given value
 // loop through the keys using Object.keys(obj)
 
@@ -19,11 +15,4 @@ const findKeyByValue = function(obj, value) {
   }
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
